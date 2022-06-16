f74211200ed1869110fa06be9f0331c40c53dd04
정민킴 <jma1020@naver.com>
2022-06-16 23:36:02 +0900

    💄 기본 컬러 추가

---
+++ src/shared/constants.ts
@@ -0,0 +1,11 @@
+export const COLOR = {
+  white: "#ffffff",
+  main: "#F25835",
+  grey: {
+    f2: "#F2F2F2",
+    d9: "#D9D9D9",
+    b3: "#B3B3B3",
+    73: "#737373",
+  },
+  black: "#000000",
+};
