0ce7f4b1edd3c5636fbe1c78cf8f4253629d1b90
jeongmin Kim <jma1020@naver.com>
2022-07-16 14:15:29 +0900

    ⚡️ modal reducer 추가

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 import exampleCounterReducer from "./slice/exampleCounterSlice";
+import modalReducer from "./slice/modalSlice";
 
 const makeStore = () =>
-  configureStore({ reducer: { exampleCounter: exampleCounterReducer } });
+  configureStore({
+    reducer: {
+      exampleCounter: exampleCounterReducer,
+      modal: modalReducer,
+    },
+  });
 
 const wrapper = createWrapper(makeStore);
 
