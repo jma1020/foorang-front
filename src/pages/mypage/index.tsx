73355f9cfb777e1f5b27c584772a5c5e130e7faf
jeongmin Kim <jma1020@naver.com>
2022-07-16 13:41:09 +0900

    ⚡️ 리팩토링한 코드 적용

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -3,6 +3,8 @@ import type { NextPage } from "next";
 import Navigator from "../../components/Navigator";
 import SettingIcon from "public/images/settingIcon.svg";
 import { useState } from "react";
+import BookMark from "src/components/myPage/BookMarkContainer";
+import Review from "src/components/myPage/ReviewContainer";
 
 const Mypage: NextPage = () => {
   const [toggle, setToggle] = useState(false);
@@ -22,36 +24,7 @@ const Mypage: NextPage = () => {
         <TapBookMark>북마크</TapBookMark>
         <TapReview>리뷰</TapReview>
       </TapContainer>
-      {toggle ? (
-        <BookMarkContainer>
-          <BookMarkImg img="https://previews.123rf.com/images/kaisorn/kaisorn1509/kaisorn150900001/44704238-%EC%A4%91%EA%B5%AD-%EC%9D%8C%EC%8B%9D%EC%9D%98-%EC%84%B8%ED%8A%B8.jpg" />
-          <BookMarkTextContainer>
-            <BookMarkStoreName>카페 허니비</BookMarkStoreName>
-            <BookMarkCategory>디저트 음료</BookMarkCategory>
-            <BookMarkStarBox />
-            <BookMarkDescription>
-              달콤한 허니비 한잔 어때요?
-            </BookMarkDescription>
-          </BookMarkTextContainer>
-        </BookMarkContainer>
-      ) : (
-        <ReviewContainer>
-          <ReviewStoreName>카페 허니비</ReviewStoreName>
-          <ReviewCategory>디저트 음료</ReviewCategory>
-          <ReviewStarBox>임시 별</ReviewStarBox>
-          <ReviewImgContainer>
-            <ReviewImg img="https://previews.123rf.com/images/kaisorn/kaisorn1509/kaisorn150900001/44704238-%EC%A4%91%EA%B5%AD-%EC%9D%8C%EC%8B%9D%EC%9D%98-%EC%84%B8%ED%8A%B8.jpg" />
-            <ReviewImg img="https://previews.123rf.com/images/kaisorn/kaisorn1509/kaisorn150900001/44704238-%EC%A4%91%EA%B5%AD-%EC%9D%8C%EC%8B%9D%EC%9D%98-%EC%84%B8%ED%8A%B8.jpg" />
-            <ReviewImg img="https://previews.123rf.com/images/kaisorn/kaisorn1509/kaisorn150900001/44704238-%EC%A4%91%EA%B5%AD-%EC%9D%8C%EC%8B%9D%EC%9D%98-%EC%84%B8%ED%8A%B8.jpg" />
-          </ReviewImgContainer>
-          <ReviewTitle>최근 먹어본 디저트 중 최고</ReviewTitle>
-          <ReviewDescription>
-            복숭아 무화과 아이스크림 베이스에 리치크림을 더한 시즌 크로플 최근
-            먹어본 디저트 중 최고 최근 먹어본 디저트 중 최고 최근 먹어본 디저트
-            중 최고 최근 먹어본 디저트 중 최고
-          </ReviewDescription>
-        </ReviewContainer>
-      )}
+      {toggle ? <BookMark /> : <Review />}
       <Navigator />
     </div>
   );
@@ -120,100 +93,6 @@ const TapReview = styled.div`
   flex-grow: 1;
   border-bottom: 2px solid #f7510b;
   padding-bottom: 0.75rem;
-`;
-
-const BookMarkContainer = styled.div`
-  display: flex;
-  padding: 2.125rem 1.375rem;
-  position: relative;
-  &::after {
-    content: "화살";
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-2.125rem, 2.375rem);
-  }
-`;
-
-const BookMarkImg = styled.div<StyledImgProps>`
-  width: 6.375rem;
-  height: 6.375rem;
-  background-image: url(${(props) => props.img});
-  background-size: 100%;
-  background-repeat: no-repeat;
-  border-radius: 0.375rem;
-  margin-right: 1.5rem;
-`;
-
-const BookMarkTextContainer = styled.div`
-  display: grid;
-  grid-template-rows: 0.5fr 0.5fr 0.5fr 0.5fr;
-`;
-
-const BookMarkStoreName = styled.h3`
-  font-size: 1.125rem;
-  font-weight: bold;
-`;
-
-const BookMarkCategory = styled.em`
-  font-size: 0.625rem;
-  color: #969696;
-`;
-
-const BookMarkStarBox = styled.div``;
-
-const BookMarkDescription = styled.p`
-  font-size: 0.75rem;
-  font-weight: 500;
-  color: #464646;
-
-  align-self: flex-end;
-`;
-
-const ReviewContainer = styled.div`
-  padding: 1.375rem;
-`;
-
-const ReviewStoreName = styled.h3`
-  font-size: 1rem;
-  font-weight: bold;
-`;
-
-const ReviewCategory = styled.em`
-  display: inline-block;
-  font-size: 0.625rem;
-  color: #969696;
-  margin-bottom: 0.875rem;
-`;
-
-const ReviewStarBox = styled.div``;
-
-const ReviewImgContainer = styled.div`
-  display: grid;
-  gap: 0.5rem;
-  grid-template-columns: repeat(3, minmax(0, 1fr));
-  margin-bottom: 0.875rem;
-`;
-
-const ReviewImg = styled.div<StyledImgProps>`
-  width: 6.375rem;
-  height: 6.375rem;
-  background-image: url(${(props) => props.img});
-  background-size: 100%;
-  background-repeat: no-repeat;
-  border-radius: 0.375rem;
-`;
-
-const ReviewTitle = styled.h3`
-  font-size: 0.875rem;
-  font-weight: bold;
-  margin-bottom: 0.25rem;
-`;
-
-const ReviewDescription = styled.p`
-  font-size: 0.75rem;
-  font-weight: 500;
-  color: #959595;
 `;
 
 export default Mypage;
