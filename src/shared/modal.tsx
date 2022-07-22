14a139d01ad311547a449fd414d0f3bcf4d96d96
jeongmin Kim <jma1020@naver.com>
2022-07-22 12:33:57 +0900

    ⚡️ Styled 파일 리팩토링

--- src/shared/modal.tsx
+++ src/shared/modal.tsx
@@ -1,9 +1,8 @@
-import styled from "@emotion/styled";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "store";
-import { open, close } from "store/slice/modalSlice";
-import { COLOR } from "./constants";
+import { close } from "store/slice/modalSlice";
+import * as Styled from "./modal.styled";
 
 interface ModalProps {
   children: React.ReactNode;
@@ -17,49 +16,18 @@ const Modal = ({ children }: ModalProps) => {
     <>
       {isModal && (
         <React.Fragment>
-          <BackGround onClick={() => dispatch(close())} />
-          <ModalBox>
+          <Styled.BackGround onClick={() => dispatch(close())} />
+          <Styled.ModalBox>
             {children}
-            <BtnContainer></BtnContainer>
-          </ModalBox>
+            <Styled.BtnContainer>
+              <Styled.CloseBtn>닫기</Styled.CloseBtn>
+              <Styled.SubmitBtn>확인</Styled.SubmitBtn>
+            </Styled.BtnContainer>
+          </Styled.ModalBox>
         </React.Fragment>
       )}
     </>
   );
 };
 
-const BackGround = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 100;
-  width: 100vw;
-  height: 100vh;
-  background-color: #555;
-`;
-
-const ModalBox = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  z-index: 200;
-  transform: translate(-50%, -50%);
-  padding: 3rem;
-  background-color: white;
-  border-radius: 1rem;
-`;
-
-const BtnContainer = styled.div`
-  display: flex;
-`;
-
-const CloseBtn = styled.button`
-  color: white;
-  background-color: ${COLOR.main};
-`;
-
-const SubmitBtn = styled.button`
-  color: white;
-  background-color: ${COLOR.main};
-`;
 export default Modal;
